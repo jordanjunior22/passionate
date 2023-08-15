905843d5cb4e7d9060a318e853f824561dddccee
jordanjunior22 <114837464+jordanjunior22@users.noreply.github.com>
2023-08-15 12:26:30 -0700

    new

--- next.config.js
+++ next.config.js
@@ -5,15 +5,7 @@ const nextConfig = {
   },
   output: "export",
   
-//   async redirects() {
-//       return [
-//           {
-//               source: '/',
-//               destination: '/home',
-//               permanent: true,
-//           },
-//       ];
-//   },
+
 };
   
 module.exports = nextConfig;
