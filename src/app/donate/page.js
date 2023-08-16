f278707cdb1c952baaf11750c78df88419874b07
jordanjunior22 <114837464+jordanjunior22@users.noreply.github.com>
2023-08-16 15:26:56 -0700

    update

--- src/app/donate/page.js
+++ src/app/donate/page.js
@@ -1,8 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
-
+import '../programs/residencial/residencial.css'
 function page() {
   return (
-    <div>Donate Page</div>
+    <div className='add-padding'>
+        <div className='residencial flex-reverse'>    
+            <img src='../images/image1.jpg' alt='image1' className='margintop'/>
+            <div className='heading'>
+                <p className='first-text set-opacity'>WE'RE IN THIS TOGETHER</p>
+                <h2 className='h2-tags'>Get Involved With Hopewell House</h2>
+            </div> 
+        </div>   
+
+        <div className='details'>
+            <h2 className='h2-tags'>Donations </h2>
+            <p className='set-opacity'>Hopewell House for Independent Living is a non-profit 501c3 organization, 
+              which means all donations are tax deductible. We immensely appreciate our donors and are deeply grateful for their support.
+              Private donations are always appreciated as they assist to provide quality services and scholarships 
+              to our residents. If you would like more information regarding the best way for you to donate 
+              to Hopewell House, please contact us.</p>
+        </div>
+
+        <div className='donate-button'>
+            <button>Donate</button>
+        </div>
+    </div>
   )
 }
 
