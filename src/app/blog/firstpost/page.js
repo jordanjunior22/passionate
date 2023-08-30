f4d44232f5f43680cb0f9fa6e70dd2fb7a062fe4
jordanjunior22 <114837464+jordanjunior22@users.noreply.github.com>
2023-08-30 12:26:48 -0700

    updates

---
+++ src/app/blog/firstpost/page.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../post.css'
+function page() {
+  return (
+    <section>
+      <div className='container'>
+        <h2 className='post-title'>Title Of Post</h2>
+
+        <div className='post-container'>
+            <div className='image-container'>
+                <img src='../images/pots.jpg' alt='images'/>
+            </div>
+            <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        
+
+      </div>
+    </section>
+  )
+}
+
+export default page
---
+++ src/app/blog/page.js
@@ -0,0 +1,56 @@
+"use client"
+import React from 'react'
+import './blog.css'
+
+function page() {
+  const FirstPost = () => {
+    // Redirect the user to the specified link
+    window.open('/blog/firstpost', '_blank');
+  };
+  const SecondPost = () => {
+    // Redirect the user to the specified link
+    window.open('/blog/secondpost', '_blank');
+  };
+  return (
+    <section>
+    <h2 className='title'>BlOG <span style={{color:'#76C63E'}}>POST</span></h2>
+      <div className='parent '>
+
+        <div className='blog-container border-gradient border-gradient-purple'>
+          <div className='image-container'> 
+            <img src='../images/pots.jpg' alt='blogimage'/>
+          </div>
+
+          <div className='blog-des'>
+            <h3 className='blog-title'>This is the tile of the blog post</h3>
+            <p className='set-opacity'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+              nisi ut aliquip ex ea commodo consequat.</p>
+            <button onClick={FirstPost}>See Post</button>
+          </div>
+        </div>
+
+        <div className='blog-container border-gradient border-gradient-purple'>
+          <div className='image-container'> 
+            <img src='../images/blog2.jpg' alt='blogimage'/>
+          </div>
+
+          <div className='blog-des'>
+          <h3 className='blog-title'>This is the tile of the blog post</h3>
+            <p className='set-opacity'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+              nisi ut aliquip ex ea commodo consequat.</p>
+            <button onClick={SecondPost}>See Post</button>
+          </div>
+        </div>
+
+      </div>
+   
+
+    </section>
+  )
+}
+
+export default page
