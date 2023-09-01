e3a55c76d6dce22da4ba195c9dcf256fc80a13b4
Flavius Prahlad <Flaspraz42@yahoo.co.uk>
2023-09-01 18:06:50 -0400

    Blogupdates

--- src/app/blog/firstpost/page.js
+++ src/app/blog/firstpost/page.js
@@ -4,24 +4,32 @@ function page() {
   return (
     <section>
       <div className='container'>
-        <h2 className='post-title'>Title Of Post</h2>
+        <h2 className='post-title'>About Mental Health</h2>
 
         <div className='post-container'>
             <div className='image-container'>
                 <img src='../images/pots.jpg' alt='images'/>
             </div>
-            <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p className='description'>Mental health refers to a person&apos;s psychological, social, and emotional well-being. 
+                Just like physical health, mental health is something everybody can actively work on. 
+                A mental illness is any disorder in the brain that affects mood, thinking or behavior, believed to be caused by genetic, biological, and environmental factors. 
+                In the United States, about 1 in 4 adults and 1 in 5 children have a mental illness in any given year.                         
+                </p>
+                <p className='description'>
+                The most common mental illnesses include anxiety, depression, obsessive-compulsive disorder (OCD), bipolar disorder, and schizophrenia. 
+                There are a number of treatment options available such as medication, lifestyle changes, different forms of therapy, and procedures depending on the type and severity of the condition. 
+                Recovery is not linear, so each individual with a mental illness must work to find the unique treatment plan that is best for them.                  
+                </p>
+                <p className='description'>
+                Mental illnesses may affect a person&apos;s ability to function, relate to, and communicate with others. 
+                The condition may be temporary, lasting a few months or years, or it may be chronic and affect the person their entire life. 
+                Early intervention and treatment are often helpful in managing a mental illness.   
+                </p>
+                <p className='description'>
+                There are a number of resources available to provide support and information to people living with mental illnesses. 
+                If you or someone you know needs help, visit the <a href="https://www.themighty.com/suicide-prevention-resources/">suicide prevention resources</a> page
+                If you are in the U.S. and need support right now, call the <a href="https://988lifeline.org/">988 Suicide & Crisis Lifeline</a> at <a href="1-800-273-8255">1-800-273-8255</a> or <a href="988">988</a>.
+                </p>
         </div>
         
 
--- src/app/blog/secondpost/page.js
+++ src/app/blog/secondpost/page.js
@@ -4,24 +4,28 @@ function page() {
   return (
     <section>
       <div className='container'>
-        <h2 className='post-title'>Title Of Post</h2>
+        <h2 className='post-title'>Top Mental Health Blogs to Read for Total Wellness</h2>
 
         <div className='post-container'>
             <div className='image-container'>
                 <img src='../images/blog2.jpg' alt='images'/>
             </div>
-            <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p className='description'><h4>There are thousands of mental health bloggers out there,
+              but these top 100 mental health blogs are particularly powerful sources of insight, wisdom, and support. 
+              Here are the most high ranking blogs among the world&apos;s long list of anxiety, bipolar disorder, depression, 
+              eating disorder, OCD, PTSD blogs, and more.</h4>
+            </p>
+            <p className='description'>
+            This ultimate list is curated with the top bloggers who have successfully used their stories to dismantle stigma, change lives, and rewrite the mental health narrative. 
+            The following blogs feature content on a variety of mental health topics which include everything from depression blogs to OCD blogs to PTSD blogs and other mental health blogs.
+            </p>
+            <p className='description'>
+            If you are looking for motivation, information, or simply a supportive online community, the following mental health blogs are an excellent place to start. 
+            With content from the best anxiety bloggers, depression bloggers, OCD bloggers, PTSD bloggers, and Schizophrenia bloggers, this ultimate list of mental health blogs is sure to help inspire wellness now and beyond.
+            </p>
+            <p className='description'>
+                Text
+            </p>  
         </div>
         
 
