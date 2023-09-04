736a19d25863c13b56625bd5f4f12ca8c9ea31a0
Jordan Junior <114837464+jordanjunior22@users.noreply.github.com>
2023-09-04 09:38:31 -0700

    Update page.js

--- src/app/about/gallery/page.js
+++ src/app/about/gallery/page.js
@@ -10,47 +10,47 @@ function page() {
             <div className='flex-column'>
               <img src='../images/image1.jpg' alt='image1'/>
               <h4 className='text-center'>Front Pouch</h4>
-              <p className='add-opacity text-center'>"An open-air haven of serenity, where nature's beauty envelops and laughter mingles with the gentle rustling of leaves."</p>
+              <p className='add-opacity text-center'>&quot;An open-air haven of serenity, where nature&apos;s beauty envelops and laughter mingles with the gentle rustling of leaves.&quot;</p>
             </div>
             <div className='flex-column'>
               <img src='../images/image2.jpg' alt='image1'/>
               <h4 className='text-center'>Living Room</h4>
-              <p className='add-opacity text-center'>"A warm and welcoming sanctuary where relaxation and connection flourish, making cherished memories with your peers."</p>
+              <p className='add-opacity text-center'>&quot;A warm and welcoming sanctuary where relaxation and connection flourish, making cherished memories with your peers.&quot;</p>
             </div>
             <div className='flex-column'>
               <img src='../images/image3.jpg' alt='image1'/>
               <h4 className='text-center'>Dinning</h4>
-              <p className='add-opacity text-center'>"An elegant space where conversations flow as effortlessly as the delicious meals, uniting friends and peers."</p>
+              <p className='add-opacity text-center'>&quot;An elegant space where conversations flow as effortlessly as the delicious meals, uniting friends and peers.&quot;</p>
             </div>
             <div className='flex-column'>
               <img src='../images/image1.jpg' alt='image1'/>
               <h4 className='text-center'>Kitchen</h4>
-              <p className='add-opacity text-center'>"The heart of the home, where culinary creativity dances with the aroma of shared meals, bringing peers together."</p>
+              <p className='add-opacity text-center'>&quot;The heart of the home, where culinary creativity dances with the aroma of shared meals, bringing peers together.&quot;</p>
             </div>
             <div className='flex-column'>
               <img src='../images/image2.jpg' alt='image1'/>
               <h4 className='text-center'>Bedroom I</h4>
-              <p className='add-opacity text-center'>"A magical kingdom where imagination reigns supreme, and the future is painted with the vibrant colors of peace and thought."</p>
+              <p className='add-opacity text-center'>&quot;A magical kingdom where imagination reigns supreme, and the future is painted with the vibrant colors of peace and thought.&quot;</p>
             </div>
             <div className='flex-column'>
               <img src='../images/image3.jpg' alt='image1'/>
               <h4 className='text-center'>Bedroom II</h4>
-              <p className='add-opacity text-center'>"A gracious haven, offering comfort to treasured privacy, creating lasting impressions of peace and healing."</p>
+              <p className='add-opacity text-center'>&quot;A gracious haven, offering comfort to treasured privacy, creating lasting impressions of peace and healing.&quot;</p>
             </div>
             <div className='flex-column'>
               <img src='../images/image1.jpg' alt='image1'/>
               <h4 className='text-center'>Bedroom III</h4>
-              <p className='add-opacity text-center'>"A tranquil retreat, cocooned in comfort, where dreams take flight and the spirit finds solace."</p>
+              <p className='add-opacity text-center'>&quot;A tranquil retreat, cocooned in comfort, where dreams take flight and the spirit finds solace.&quot;</p>
             </div>
             <div className='flex-column'>
               <img src='../images/image2.jpg' alt='image1'/>
               <h4 className='text-center'>Hallway</h4>
-              <p className='add-opacity text-center'>"A graceful bridge between rooms, where the echoes of shared moments reverberate through thoughts."</p>
+              <p className='add-opacity text-center'>&quot;A graceful bridge between rooms, where the echoes of shared moments reverberate through thoughts.&quot;</p>
             </div>
             <div className='flex-column'>
               <img src='../images/image3.jpg' alt='image1'/>
               <h4 className='text-center'>Bathroom</h4>
-              <p className='add-opacity text-center'>"A serene oasis of self-care, where stress melts away in the soothing embrace of water and tranquility."</p>
+              <p className='add-opacity text-center'>&quot;A serene oasis of self-care, where stress melts away in the soothing embrace of water and tranquility.&quot;</p>
             </div>
 
           </div>
