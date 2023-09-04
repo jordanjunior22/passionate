cfc993bd5861fa772e9562474cc56937b42ddb57
Jordan Junior <114837464+jordanjunior22@users.noreply.github.com>
2023-09-04 10:00:25 -0700

    contact page.js

---
+++ src/app/contact/page.js
@@ -0,0 +1,16 @@
+"use client"
+import ContactForm from '@/components/ContactForm'
+import Map from '@/components/Map'
+import React from 'react'
+
+
+function page() {
+  return (
+    <section>
+        <ContactForm/>
+        <Map/>
+    </section>
+  )
+}
+
+export default page
